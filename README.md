5d5ca78bb0ec02e4167e066c41620b394abead59
melany estevez camata <elibialany@gmail.com>
2023-02-04 13:53:28 -0400

    final back && front

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# BoilerplateApp
-boilerplatefullstackapp
+Add .env:
+```
+PORT=4000
+REACT_APP_BACKEND_URL=http://localhost:4000
+```
---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import axiosClient from './config/axios';
+
+function App() {
+  const [users, setUsers] = useState([])
+  const url = '/user'
+  useEffect(() => {
+    const fetch = async () => {
+      await axiosClient.get(url).then(res => setUsers(res.data)).catch(err => console.log(err))
+    }
+    fetch()
+  },[url]
+  )
+  return (
+  <div>
+    {users.map(user=>
+      <div key={user.id}>
+        <h3>{user.username}</h3>
+        <h5>{user.title}</h5>
+      </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
